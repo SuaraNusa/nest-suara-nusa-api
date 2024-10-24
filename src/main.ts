87596841325b84d3c405ad360caaa8b46317f9d0
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-25 01:39:31 +0700

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('SuaraNusa API Documentation')
+    .setDescription('SuaraNusa API Documentation - Bangkit 2024 Batch II')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
 }
+
 bootstrap();
