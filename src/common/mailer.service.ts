28f7feb39446075854dadd1d94058086ef586025
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-25 17:30:58 +0700

    fix: typing

--- src/common/mailer.service.ts
+++ src/common/mailer.service.ts
@@ -2,16 +2,12 @@ import * as nodeMailer from 'nodemailer';
 import { ConfigService } from '@nestjs/config';
 import { EmailTransfer } from '../model/email.transfer';
 import Mail from 'nodemailer/lib/mailer';
-import { Transporter } from 'nodemailer';
-import SMTPPool from 'nodemailer/lib/smtp-pool';
+import { SentMessageInfo, Transporter } from 'nodemailer';
 
 export class MailerService {
   constructor(private readonly configService: ConfigService) {}
 
-  private nodeMailerTransporter: Transporter<
-    SMTPPool.SentMessageInfo,
-    SMTPPool.Options
-  >;
+  private nodeMailerTransporter: Transporter<SentMessageInfo>;
 
   mailerTransporter() {
     if (!this.nodeMailerTransporter) {
@@ -23,13 +19,19 @@ export class MailerService {
           user: this.configService.get<string>('NODE_MAILER_USERNAME'),
           pass: this.configService.get<string>('NODE_MAILER_PASSWORD'),
         },
-      });
+      } as nodeMailer.TransportOptions);
     }
     return this.nodeMailerTransporter;
   }
 
   async dispatchMailTransfer(emailTransfer: EmailTransfer) {
     const nodeMailerTransporter = this.mailerTransporter();
+    const htmlBody = emailTransfer.placeholderReplacements
+      ? this.mailTemplate(
+          emailTransfer.html,
+          emailTransfer.placeholderReplacements,
+        )
+      : emailTransfer.html;
     const mailOptions: Mail.Options = {
       from: emailTransfer.from ?? {
         name: this.configService.get<string>('NODE_MAILER_APP_NAME'),
@@ -37,12 +39,21 @@ export class MailerService {
       },
       to: emailTransfer.recipients,
       subject: emailTransfer.subject,
-      html: emailTransfer.html,
+      html: htmlBody,
     };
     try {
       return await nodeMailerTransporter.sendMail(mailOptions);
     } catch (err) {
       console.log(err);
     }
+  }
+
+  mailTemplate(htmlBody: string, replacementsMap: Record<string, string>) {
+    return htmlBody.replace(
+      /%(\w*)%/g, // or /{(\w*)}/g for "{this} instead of %this%"
+      function (m, key) {
+        return replacementsMap.hasOwnProperty(key) ? replacementsMap[key] : '';
+      },
+    );
   }
 }
