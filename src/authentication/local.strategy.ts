486656cfd0fa158e992d7d1b8a411042af2aec78
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-25 14:09:19 +0700

    refactor: change variable naming

--- src/authentication/local.strategy.ts
+++ src/authentication/local.strategy.ts
@@ -3,6 +3,7 @@ import { AuthenticationService } from './authentication.service';
 import { UserCredentials } from './dto/user-credentials.dto';
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
+import { LoggedUser } from './dto/logged-user.dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -10,12 +11,12 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super({ usernameField: 'email' });
   }
 
-  async validate(userCredentials: UserCredentials): Promise<any> {
-    const user =
+  async validate(userCredentials: UserCredentials): Promise<LoggedUser> {
+    const loggedUser: LoggedUser =
       await this.authenticationService.validateUserCredentials(userCredentials);
-    if (!user) {
+    if (!loggedUser) {
       throw new UnauthorizedException();
     }
-    return user;
+    return loggedUser;
   }
 }
