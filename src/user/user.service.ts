7ad443c8a20c56de0146f961aece43f2cadb513e
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-30 22:11:01 +0700

    refactor: delete unnecessary func

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import PrismaService from '../common/prisma.service';
-import ValidationService from '../common/validation.service';
-import { AuthenticationValidation } from '../authentication/authentication.validation';
-import * as bcrypt from 'bcrypt';
-import { UserCredentials } from '../authentication/dto/user-credentials.dto';
-import { LoggedUser } from '../authentication/dto/logged-user.dto';
 
 @Injectable()
 export class UserService {
-
-
   findAll() {
     return `This action returns all user`;
   }
