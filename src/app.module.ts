e061fce62df0fd8db6d296edf7f3e9e2886a7f58
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-30 22:09:50 +0700

    feat: import and provide modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
+import { MailerService } from './common/mailer.service';
 
 @Module({
-  imports: [UserModule, AuthenticationModule, CommonModule],
+  imports: [
+    UserModule,
+    AuthenticationModule,
+    CommonModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 import PrismaService from './prisma.service';
 import ValidationService from './validation.service';
+import { MailerService } from './mailer.service';
 
 @Module({
-  imports: [],
-  providers: [PrismaService, ValidationService],
-  exports: [PrismaService, ValidationService],
+  imports: [
+    WinstonModule.forRoot({
+      level: 'debug',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple(),
+          ),
+        }),
+      ],
+    }),
+  ],
+  providers: [PrismaService, ValidationService, MailerService],
+  exports: [PrismaService, ValidationService, MailerService],
 })
 export class CommonModule {}
