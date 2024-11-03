f310e7cfaed6a8f08efb0e86ce11027006179208
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-03 09:28:17 +0700

    feat: check confirm password

--- src/authentication/authentication.validation.ts
+++ src/authentication/authentication.validation.ts
@@ -5,15 +5,27 @@ export class AuthenticationValidation {
     email: z.string().email().max(100),
     password: z.string(),
   });
-  static SIGN_UP: ZodType = z.object({
-    name: z.string().min(5).max(100),
-    email: z.string().email().min(1).max(255),
-    password: z.string().min(1).max(100),
-    verification_questions: z.array(
-      z.object({
-        verificationQuestionId: z.number().min(1),
-        answer: z.string().min(1).max(200),
-      }),
-    ),
-  });
+  static SIGN_UP: ZodType = z
+    .object({
+      name: z.string().min(5).max(100),
+      email: z.string().email().min(1).max(255),
+      password: z.string().min(1).max(100),
+      confirm_password: z.string().min(1).max(100),
+      verification_questions: z.array(
+        z.object({
+          verificationQuestionId: z.number().min(1),
+          answer: z.string().min(1).max(200),
+        }),
+      ),
+      isVerified: z.boolean().optional(),
+    })
+    .superRefine(({ confirm_password, password }, ctx) => {
+      if (confirm_password !== password) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'The passwords did not match',
+          path: ['confirm_password'],
+        });
+      }
+    });
 }
--- src/authentication/dto/sign-up.dto.ts
+++ src/authentication/dto/sign-up.dto.ts
@@ -2,6 +2,7 @@ export class SignUpDto {
   name: string;
   email: string;
   password: string;
+  confirm_password: string;
   verification_questions: [
     {
       verificationQuestionId: number;
