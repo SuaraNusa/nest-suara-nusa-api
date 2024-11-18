050b7c8a7415c0972e91af13633c7c5d09fb19d0
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-18 18:17:19 +0700

    fix: minor bug

--- src/instrument/instrument.module.ts
+++ src/instrument/instrument.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InstrumentService } from './instrument.service';
 import { InstrumentController } from './instrument.controller';
+import { CommonModule } from '../common/common.module';
 
 @Module({
+  imports: [CommonModule],
   controllers: [InstrumentController],
   providers: [InstrumentService],
 })
